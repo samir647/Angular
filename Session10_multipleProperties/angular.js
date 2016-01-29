8842c7a2881e539281c64d894ad04117e2fedae7
samir shrestha <sam.samirshrestha@gmail.com>
2016-01-29 12:23:47 -0600

    Commit to Branch Samir

--- Session10_multipleProperties/angular.js
+++ Session10_multipleProperties/angular.js
@@ -1,18 +1,28 @@
 
 	var myApp=angular
 		.module("myModule",[])
+		.filter
 		.controller("myController", function($scope){
 		var employees =[{name:"Samir",City:"Kathmadnu",Gender:"Male"},
 						 {name:"Ram",	City:"Bhaktapur",Gender:"Male"},
 						 {name:"Sita",City:"Pokhara",Gender:"Female"},
 						 {name:"Hari",City:"Kathmadnu",Gender:"Male"},
 						];
-		
 			$scope.employees=employees;
-		
-			
-			
-			
+			$scope.search = function(item){
+				if($scope.SearchText== undefined){
+					return true;
+					}
+				else{
+					if((item.name.toLowerCase().indexof($scope.searchText.toLowerCase())!= -1)||
+					(item.City.toLowerCase().indexof($scope.searchText.toLowerCase())!=-1))
+					
+						{
+						return true;
+						}
+					}
+					return false;
+				}
 			
 			});
 		
